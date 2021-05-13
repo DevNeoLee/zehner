29ce8b780948f76535a2a6c821c197c8bdda3360
Justin K Lee <44183510+DevNeoLee@users.noreply.github.com>
2021-05-13 09:39:05 -0700

    Add keyboard keydown focus features

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /* selectors */
 //shop, side menues
 const shop_button = document.querySelector('#shop');
-const side_menu = document.querySelector('.fa-stack');
-const side_menu_content = document.querySelector('.side-menu-container')
+const side_menu = document.querySelector('#main-menu');
+const side_menu_content = document.querySelector('#side-menu-container')
 const side_menu_close = document.querySelector('.fas.fa-times');
+
+//side menu items
+const side_menu_items = document.querySelectorAll('#side-menu-container ul li')
 
 //searchbar
 const searchbar_search_button = document.querySelector('#search');
@@ -14,13 +17,16 @@ const searchbar_close_button = document.querySelector('#close-icon');
 const country_button = document.querySelector('#country-label-container');
 const country_options = document.querySelector('#country-options');
 const country_label = document.querySelector('#country-label');
+const country_label_link = document.querySelector('#country-label-container a');
 
 //country options box
 const countries = document.querySelectorAll('#country-options ul li');
 
-console.log(countries);
+// document.querySelectorsAll('a').addEventlistener('click', (e) => {
+//   e.preventDefault();
+// })
 
-/* calling addEventListeners */
+/* calling all addEventListeners */
 side_menu_toggle();
 searchbar_show();
 searchbar_close();
@@ -30,18 +36,23 @@ country_select();
 /* functions */
 //side menues
 function side_menu_toggle() {
-  shop_button.addEventListener('click', () => {
+  shop_button.addEventListener('click', (e) => {
+    e.preventDefault();
     toggle_sidemenu()
+    
   })
 
-  side_menu.addEventListener('click', () => {
+  side_menu.addEventListener('click', (e) => {
+    e.preventDefault();
     toggle_sidemenu()
   })
 
   function toggle_sidemenu() {
-       if (side_menu_content.classList.contains('hide_display')) {
+    if (side_menu_content.classList.contains('hide_display')) {
       side_menu_content.classList.remove('hide_display');
       side_menu_close.classList.remove('hide_display');
+      console.log(side_menu_items.item(0))
+      side_menu_items.item(0).focus();
     }
     else {
       side_menu_content.classList.add('hide_display');
@@ -52,64 +63,99 @@ function side_menu_toggle() {
 
 //searchbar 
 function searchbar_show() {
-  searchbar_search_button.addEventListener('click', () => {
+  searchbar_search_button.addEventListener('click', (e) => {
+    e.preventDefault();
     searchbar.classList.remove('hide_display');
   })
 }
 
 function searchbar_close() {
-  searchbar_close_button.addEventListener('click', () => {
+  searchbar_close_button.addEventListener('click', (e) => {
+    e.preventDefault();
     searchbar.classList.add('hide_display');
   })
 }
 
 //country button
 function country_button_toggle() {
-  country_button.addEventListener('click', () => {
+  country_button.addEventListener('click', (e) => {
+    e.preventDefault(e);
     country_options.classList.toggle('hide_display');
+    countries.item(0).focus();
   })
 }
 
+//country box mouse select feature
 function country_select() {
   let selected = null;
 
-   countries.forEach(country => {
+   countries.forEach((country, index )=> {
      country.addEventListener('click', () => {
-        console.log(country.textContent);
         country_label.textContent = country.textContent;
         country_options.classList.toggle('hide_display');
+        country_label_link.focus();
      })
 
-      country.addEventListener('keydown', (e) => {
-        // country_options.classList.toggle('hide_display');
-        console.log(e)
+      country.addEventListener('keydown', e => {
+        handleKeydown(e, country, index, 12);
       })
-
    })
+}
 
-  //  countries.addEventListener('escape', () => {
-  //    country_options.classList.toggle('hide_display');
-  //  })
+//preventing default window scroll behaviors on country_options box
+country_options.addEventListener("keydown", function(e) {
+    if(["ArrowUp","ArrowDown"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
+//keyboard control of the menu
+function handleKeydown(event, country, index, height) {
+  switch(event.key) {
+    case 'Enter':
+      country_label.textContent = country.textContent;
+      country_options.classList.toggle('hide_display');
+      country_label_link.focus();
+      break;
+    case 'Escape':
+      country_options.classList.toggle('hide_display');
+      country_label_link.focus();
+      break;
+    case 'ArrowUp':
+      if ( index > 0) {
+        event.target.previousElementSibling.focus();
+        countries.item(index).blur();
+        event.target.previousElementSibling.focus();
+        country_options.scrollBy(0, -height);
+        index = index - 1;
+      } 
+      break;
+    case 'ArrowDown':
+      if (countries.item(0) === event.target) {
+        countries.item(0).blur();
+        event.target.nextElementSibling.focus();
+        country_options.scrollBy(0, height);
+        index +=1;
+      } else if ( index < countries.length - 1) {
+        event.target.nextElementSibling.focus();
+        country_options.scrollBy(0, height);
+        index +=1;
+      } 
+      break;
+    default:
+      country_options.classList.toggle('hide_display');
+      country_label_link.focus();   
+  }
+}
+
+mouse_click_outside();
+//mouse outside click menu box close feature
+function mouse_click_outside() {
+  // console.log(countries.all.blur())
+  console.log(countries.hasOwnProperty(Event));
+  // if (countries.(e.target).length === 0) {
+  //   country_options.classList.toggle('hide_display');
+  // }
+ 
+}
   
-}
-
-document.querySelector('.header').addEventListener('keydown', e => {
-    console.log(e);
-}) 
-
-function Element(element) {
-  this.element = element;
-  this.value = element.textContent; 
-}
-
-// const element = new Element(document.querySelector('#about_4moms'));
-
-// console.log(element)
-// element.textContent.addEventListener('click', e => {
-//   console.log(e);
-// });
-
-document.querySelector('#about_4moms a').addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log('hello');
-});
